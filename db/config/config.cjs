53c5fcdb7187b860b10aebb213c0c5718356347b
rolanq <rolan.oyun@gmail.com>
2025-04-02 12:37:46 +0300

    disabled ssl for prod

--- db/config/config.cjs
+++ db/config/config.cjs
@@ -24,11 +24,11 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false,
+    //   },
+    // },
   },
 };
