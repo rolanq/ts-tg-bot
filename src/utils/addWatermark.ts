2b3e25a0229a5d3d95dd1366c2266d908f3be742
rolanq <rolan.oyun@gmail.com>
2025-04-08 15:15:14 +0300

    changed watermark font

--- src/utils/addWatermark.ts
+++ src/utils/addWatermark.ts
@@ -2,8 +2,8 @@ import sharp from "sharp";
 import fs from "fs";
 import path from "path";
 import { promisify } from "util";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -17,21 +17,31 @@ export async function addWatermark(imageBuffer: Buffer) {
   try {
     // Получаем метаданные изображения
     const metadata = await sharp(imageBuffer).metadata();
+    const fontSize = Math.min(metadata.width!, metadata.height!) / 10;
 
     const watermarkText = `
-        <svg width="${metadata.width}" height="${metadata.height}">
-          <style>
-            .text {
-              fill: white;
-              font-size: 80px;
-              font-weight: bold;
-              opacity: 0.3;
-              font-family: Arial, sans-serif;
-            }
-          </style>
-          <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle" class="text">${process.env.WATERMARK}</text>
-        </svg>
-      `;
+    <svg width="${metadata.width}" height="${metadata.height}">
+      <style>
+        .text {
+          fill: white;
+          font-size: ${fontSize}px;
+          font-family: sans-serif;
+          opacity: 0.4;
+          transform: rotate(-45deg);
+        }
+      </style>
+      <defs>
+        <pattern id="watermark" x="0" y="0" width="${
+          metadata.width! / 2
+        }" height="${metadata.height! / 2}" patternUnits="userSpaceOnUse">
+          <text x="50%" y="50%" text-anchor="middle" class="text" dominant-baseline="middle">${
+            process.env.WATERMARK
+          }</text>
+        </pattern>
+      </defs>
+      <rect width="100%" height="100%" fill="url(#watermark)"/>
+    </svg>
+  `;
 
     const svgPath = path.join(tempDir, `watermark-${Date.now()}.svg`);
     await promisify(fs.writeFile)(svgPath, watermarkText);
